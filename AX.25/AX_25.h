c8606b80198250e050ff4cefdd2e7cc36a7e45f6
Jeremy Kitchen <kitchen@kitchen.io>
2019-05-26 13:50:19 -0700

    Initial Commit

---
+++ AX.25/AX_25.h
@@ -0,0 +1,19 @@
+//
+//  AX_25.h
+//  AX.25
+//
+//  Created by Jeremy Kitchen on 5/26/19.
+//  Copyright © 2019 Jeremy Kitchen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AX_25.
+FOUNDATION_EXPORT double AX_25VersionNumber;
+
+//! Project version string for AX_25.
+FOUNDATION_EXPORT const unsigned char AX_25VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AX_25/PublicHeader.h>
+
+

c49e6334569708abd7a2331acaa14218ba72ca11
Jeremy Kitchen <kitchen@kitchen.io>
2019-05-29 21:38:13 -0700

    add these files because they're referenced in the project

---
+++ AX.25/CallSignSSID.swift
@@ -0,0 +1,18 @@
+//
+//  CallSignSSID.swift
+//  AX.25
+//
+//  Created by Jeremy Kitchen on 5/29/19.
+//  Copyright © 2019 Jeremy Kitchen. All rights reserved.
+//
+
+import Foundation
+
+//public struct CallSignSSID {
+//    let CallSign: String
+//    let SSID: UInt8 // UInt4 if I end up pulling in that library, perhaps
+//    
+//    let SevenBit: Bool
+//    
+//    
+//}
---
+++ AX.25Tests/CallSignSSIDTests.swift
@@ -0,0 +1,13 @@
+//
+//  CallSignSSIDTests.swift
+//  AX.25Tests
+//
+//  Created by Jeremy Kitchen on 5/29/19.
+//  Copyright © 2019 Jeremy Kitchen. All rights reserved.
+//
+
+import XCTest
+
+class CallSignSSIDTests: XCTestCase {
+    
+}
